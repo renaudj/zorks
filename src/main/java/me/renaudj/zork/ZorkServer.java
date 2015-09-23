c94c53cc7061052747c7db8dfafd8f347914905d
Jordan Renaud <renaudj@windsor.k12.mo.us>
2015-09-23 14:55:06 +0000

    First

--- src/main/java/me/renaudj/zork/ZorkServer.java
+++ src/main/java/me/renaudj/zork/ZorkServer.java
@@ -2,9 +2,11 @@ package me.renaudj.zork;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ZorkServer {
   public ServerSocket serverSocket;
+  public boolean running = false;
   
   public ZorkServer(){
     start();
@@ -17,7 +19,11 @@ public class ZorkServer {
   public void start(){
     try {
         serverSocket = new ServerSocket(5000);
-      
+      running = true;
+      	while(running){
+           Socket socket = serverSocket.accept();
+           System.out.println("Connection made!");
+         }
     } catch (IOException e) {
         e.printStackTrace();
     }
